a7d3ab825fd18768ef2141fe72fe23d4c3f7d56c
Nicolas-Cardona2 <nicolas.cardona.garcia@correounivalle.edu.co>
2025-09-09 15:03:02 -0500

    la inflacion bro

--- Clase3/src/App.java
+++ Clase3/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Helao a 100 pesos");
         System.out.println("que le pasa, tan muy caros");
+        System.out.println("la inflacion mano");
     }
 }
